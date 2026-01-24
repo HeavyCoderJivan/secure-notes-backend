2fa2ba9244334bd1e74c93d32c28e3d28bcaf1bd
HeavyCoderJivan <khose.jivan01@gmail.com>
2026-01-24 17:18:49 +0530

    Added UserDetailsImpl and UserDetailsServiceImpl within securty sub package called security.services

---
+++ src/main/java/com/secure/notes/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.secure.notes.security.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.secure.notes.model.User;
+import com.secure.notes.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService
+{
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+	{
+		 User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+		 UserDetailsImpl.build(user);
+
+        return  UserDetailsImpl.build(user);
+
+		
+	}
+
+}
