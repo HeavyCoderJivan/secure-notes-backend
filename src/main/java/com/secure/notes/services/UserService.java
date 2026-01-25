a784833113350334c18380413958ae2dbd2855e1
HeavyCoderJivan <khose.jivan01@gmail.com>
2026-01-25 17:17:02 +0530

    Introduce RoleRepository, UserService, and UserServiceImpl

---
+++ src/main/java/com/secure/notes/services/UserService.java
@@ -0,0 +1,13 @@
+package com.secure.notes.services;
+import java.util.List;
+import com.secure.notes.dtos.UserDTO;
+import com.secure.notes.model.User;
+public interface UserService {
+    void updateUserRole(Long userId, String roleName);
+
+    List<User> getAllUsers();
+
+    UserDTO getUserById(Long id);
+    
+    
+}
