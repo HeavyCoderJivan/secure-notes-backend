14bb2207f0a6246ccdd8ca9bda2c2cfa9e369cb9
HeavyCoderJivan <khose.jivan01@gmail.com>
2026-01-26 15:06:30 +0530

    Implement URL-based access restrictions in SecurityConfig in class

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -1,16 +1,14 @@
 package com.secure.notes.security;
-
 import java.time.LocalDate;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
 import com.secure.notes.model.AppRole;
 import com.secure.notes.model.Role;
 import com.secure.notes.model.User;
@@ -19,11 +17,18 @@ import com.secure.notes.repositories.UserRepository;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = 
+true,securedEnabled = true,
+jsr250Enabled = true)
 public class SecurityConfig {
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		http.authorizeHttpRequests((requests) 
+				-> 
+		requests
+		.requestMatchers("/api/admin/**").hasRole("ADMIN")
+		.anyRequest().authenticated());
 		http.csrf(AbstractHttpConfigurer::disable);
 		// http.formLogin(withDefaults());
 		http.httpBasic(Customizer.withDefaults());
